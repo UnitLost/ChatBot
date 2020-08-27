299b8a978e7fdf5170bd154d237ee8dd8e809f71
tzy <985465794@qq.com>
2020-08-27 22:41:50 +0800

    arryadapter

--- app/src/main/java/com/example/chatbot/Menu.java
+++ app/src/main/java/com/example/chatbot/Menu.java
@@ -3,6 +3,9 @@ package com.example.chatbot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.example.chatbot.R;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        //要显示的数据
+        String[] strs = {"1111","2222","3333","4444","5555"};
+        //创建ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this,android.R.layout.simple_expandable_list_item_1,strs);
+        //获取ListView对象，通过调用setAdapter方法为ListView设置Adapter设置适配器
+        ListView list_test = (ListView) findViewById(R.id.list_test);
+        list_test.setAdapter(adapter);
+
     }
 }
