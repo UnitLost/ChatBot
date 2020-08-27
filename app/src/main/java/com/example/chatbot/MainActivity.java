7f0543980408b53c419a05fa35c020ca568f7583
tzy <985465794@qq.com>
2020-08-27 16:05:33 +0800

    menu activity created

--- app/src/main/java/com/example/chatbot/MainActivity.java
+++ app/src/main/java/com/example/chatbot/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.chatbot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*启动菜单activity，当点击启动按钮时调用*/
+    public void startMenu(View view){
+        Intent intent = new Intent(this, Menu.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
