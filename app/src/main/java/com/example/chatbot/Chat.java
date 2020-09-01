77986f49bcae8306c58b30c2b535357ca4404a23
tzy <985465794@qq.com>
2020-09-01 11:15:09 +0800

    chat view

--- app/src/main/java/com/example/chatbot/Chat.java
+++ app/src/main/java/com/example/chatbot/Chat.java
@@ -2,13 +2,40 @@ package com.example.chatbot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Chat extends AppCompatActivity {
+
+    private ListView list_one;
+    private MyAdapter mAdapter = null;
+    private List<Data> mData = null;
+    private Context mContext = null;
+    private Button btn_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        mContext = Chat.this;
+        bindViews();
+        mData = new LinkedList<Data>();
+        mAdapter = new MyAdapter((LinkedList<Data>) mData,mContext);
+        list_one.setAdapter(mAdapter);
     }
+
+    private void bindViews(){
+        list_one = (ListView) findViewById(R.id.list_one);
+    }
+
+    public void clickUpdate(View v){
+        mAdapter.add(new Data(R.mipmap.ic_launcher,"新消息"));
+    }
+
 }
--- app/src/main/java/com/example/chatbot/MyAdapter.java
+++ app/src/main/java/com/example/chatbot/MyAdapter.java
@@ -1,4 +1,71 @@
 package com.example.chatbot;
 
-public class MyAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+public class MyAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private LinkedList<Data> mData;
+
+    public MyAdapter() {}
+
+    public MyAdapter(LinkedList<Data> mData, Context mContext) {
+        this.mData = mData;
+        this.mContext = mContext;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if(convertView == null){
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_list,parent,false);
+            holder = new ViewHolder();
+            holder.img_icon = (ImageView) convertView.findViewById(R.id.img_icon);
+            holder.txt_content = (TextView) convertView.findViewById(R.id.txt_content);
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.img_icon.setImageResource(mData.get(position).getImgId());
+        holder.txt_content.setText(mData.get(position).getContent());
+        return convertView;
+    }
+
+    public void add(Data data) {
+        if (mData == null) {
+            mData = new LinkedList<>();
+        }
+        mData.add(data);
+        notifyDataSetChanged();
+    }
+
+    private class ViewHolder{
+        ImageView img_icon;
+        TextView txt_content;
+    }
+
 }
